f0e3350361937c9a91f23e1ed9d863b4ae1c12e3
thordaff <thordaf2305@gmail.com>
2026-03-06 10:56:34 +0700

    fix: change href type in BreadcrumbItem and NavItem to string

--- resources/js/types/navigation.ts
+++ resources/js/types/navigation.ts
@@ -1,14 +1,13 @@
-import type { InertiaLinkProps } from '@inertiajs/vue3';
 import type { LucideIcon } from 'lucide-vue-next';
 
 export type BreadcrumbItem = {
     title: string;
-    href: NonNullable<InertiaLinkProps['href']>;
+    href: string;
 };
 
 export type NavItem = {
     title: string;
-    href: NonNullable<InertiaLinkProps['href']>;
+    href: string;
     icon?: LucideIcon;
     isActive?: boolean;
     children?: NavItem[];
