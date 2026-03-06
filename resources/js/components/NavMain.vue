92e2f6194b6157626fb139956d57837f1e4a3b3f
thordaff <thordaf2305@gmail.com>
2026-03-06 10:47:34 +0700

    feat: refactor NavMain component to use RouterLink and enhance collapsible menu structure

--- resources/js/components/NavMain.vue
+++ resources/js/components/NavMain.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
+import { RouterLink } from 'vue-router';
+import { ChevronRight } from 'lucide-vue-next';
+import { ref } from 'vue';
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from '@/components/ui/collapsible';
 import {
     SidebarGroup,
     SidebarGroupLabel,
     SidebarMenu,
     SidebarMenuButton,
     SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
 } from '@/components/ui/sidebar';
 import { useCurrentUrl } from '@/composables/useCurrentUrl';
 import type { NavItem } from '@/types';
@@ -15,24 +25,70 @@ defineProps<{
 }>();
 
 const { isCurrentUrl } = useCurrentUrl();
+
+const openItems = ref<Record<string, boolean>>({});
+
+function toggleItem(title: string) {
+    openItems.value[title] = !openItems.value[title];
+}
 </script>
 
 <template>
     <SidebarGroup class="px-2 py-0">
         <SidebarGroupLabel>Platform</SidebarGroupLabel>
         <SidebarMenu>
-            <SidebarMenuItem v-for="item in items" :key="item.title">
-                <SidebarMenuButton
-                    as-child
-                    :is-active="isCurrentUrl(item.href)"
-                    :tooltip="item.title"
-                >
-                    <Link :href="item.href">
-                        <component :is="item.icon" />
-                        <span>{{ item.title }}</span>
-                    </Link>
-                </SidebarMenuButton>
-            </SidebarMenuItem>
+            <template v-for="item in items" :key="item.title">
+                <!-- Parent item with children -->
+                <SidebarMenuItem v-if="item.children?.length">
+                    <Collapsible
+                        :open="!!openItems[item.title]"
+                        @update:open="toggleItem(item.title)"
+                    >
+                        <CollapsibleTrigger as-child>
+                            <SidebarMenuButton :tooltip="item.title">
+                                <component :is="item.icon" />
+                                <span>{{ item.title }}</span>
+                                <ChevronRight
+                                    class="ml-auto transition-transform duration-200"
+                                    :class="{ 'rotate-90': !!openItems[item.title] }"
+                                />
+                            </SidebarMenuButton>
+                        </CollapsibleTrigger>
+                        <CollapsibleContent>
+                            <SidebarMenuSub>
+                                <SidebarMenuSubItem
+                                    v-for="child in item.children"
+                                    :key="child.title"
+                                >
+                                    <SidebarMenuSubButton
+                                        as-child
+                                        :is-active="isCurrentUrl(child.href)"
+                                    >
+                                        <RouterLink :to="child.href">
+                                            <component v-if="child.icon" :is="child.icon" />
+                                            <span>{{ child.title }}</span>
+                                        </RouterLink>
+                                    </SidebarMenuSubButton>
+                                </SidebarMenuSubItem>
+                            </SidebarMenuSub>
+                        </CollapsibleContent>
+                    </Collapsible>
+                </SidebarMenuItem>
+
+                <!-- Regular item without children -->
+                <SidebarMenuItem v-else>
+                    <SidebarMenuButton
+                        as-child
+                        :is-active="isCurrentUrl(item.href)"
+                        :tooltip="item.title"
+                    >
+                        <RouterLink :to="item.href">
+                            <component :is="item.icon" />
+                            <span>{{ item.title }}</span>
+                        </RouterLink>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </template>
         </SidebarMenu>
     </SidebarGroup>
 </template>
