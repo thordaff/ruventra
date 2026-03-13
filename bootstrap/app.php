14deda8e205b39fc18ab05df93061cd85c8d7b11
thordaff <thordaf2305@gmail.com>
2026-03-13 08:39:41 +0700

    feat: add EnsureSingleSession middleware to enhance session management

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\HandleAppearance;
+use App\Http\Middleware\EnsureSingleSession;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             HandleAppearance::class,
+            EnsureSingleSession::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
